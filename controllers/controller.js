a3bbf9436f83999f34371b24c8bc9a6ca4b8a98c
arvilays <123064316+arvilays@users.noreply.github.com>
2025-08-08 22:56:52 -0700

    Changed minimum password length to 3 from 6

--- controllers/controller.js
+++ controllers/controller.js
@@ -56,7 +56,7 @@ export const signup_post = [
       return true;
     })
     .escape(),
-  body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters long."),
+  body("password").isLength({ min: 3 }).withMessage("Password must be at least 3 characters long."),
   body("confirmPassword").custom((value, { req }) => {
     if (value !== req.body.password) throw new Error("Passwords do not match.");
     return true;
